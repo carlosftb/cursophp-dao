3d1bc98b1751efc560eb33a8dd1ef806c5885eaa
Carlos Silva <carlos.david.ftb@hotmail.com>
2022-07-01 10:47:42 -0300

    criação dos metodos de lista

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 
 require_once("config.php");
+//carrega um usuário
 
-$root = new Usuario();
+//$root = new Usuario();
+//$root->loadById(25);
+//cho $root;
 
-$root->loadById(25);
+//Carrega uma lista de todos os usuários
+//$lista = USUARIO::getList();
 
-echo $root;
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo Login 
+
+//$search = Usuario::search("jurandi");
+
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
 
 
+$usuario = new Usuario();
+$usuario->login("jurandi","1111");
+
+echo $usuario;
 ?>
